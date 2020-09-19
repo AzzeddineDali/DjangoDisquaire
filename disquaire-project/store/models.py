88b670264c8ba4114f60eec4e53291835430fcd3
Azzeddine Dali <azzeddinedali@outlook.com>
2020-09-19 18:25:20 +0100

    Config Media, Static files and Adding items to our base

--- disquaire-project/store/models.py
+++ disquaire-project/store/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Album(models.Model):
@@ -18,9 +24,15 @@ class Album(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='album_pics')
     artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Booking(models.Model):
     contacted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
     album = models.OneToOneField(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contact.name
